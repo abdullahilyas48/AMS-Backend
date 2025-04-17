29c3fd157816d358173ff7f3737035940538abe0
Muhammad Abdullah <abdullahilyas48@gmail.com>
2025-04-17 20:57:19 +0500

    Update index.js

--- index.js
+++ index.js
@@ -9,8 +9,26 @@ app.use(cors())
 mongoose.connect("mongodb://127.0.0.1:27017/User")
 
 app.listen(3001,()=>{
-    UserModel.create(req.body).then(users => res.json(users)).catch(err=> res.json(err))
+    
 })
-app.post('/register', (req,res) => {
 
+app.post('/login', (req,res) => {
+    const {email, password} = req.body;
+    UserModel.findOne({email: email})
+    .then(user => {
+        if(user){
+            if(user.password === password) {
+                res.json("Success!")
+            } else {
+                res.json("Wrong Password.")
+            }
+        } else {
+            res.json("No Records Exist.")
+        }
+    })
+    .catch(err => res.json(err))
+})
+
+app.post('/register', (req,res) => {
+    UserModel.create(req.body).then(users => res.json(users)).catch(err=> res.json(err))
 })
