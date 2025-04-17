14f02dcb5c9d4975c1cec802a2ebfa06c535d978
Muhammad Abdullah <abdullahilyas48@gmail.com>
2025-04-17 21:03:53 +0500

    Admin Login

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express")
 const mongoose = require("mongoose")
 const cors = require ('cors')
 const UserModel = require('./models/User')
+const AdminModel = require('./models/Admin')
 const app = express()
 app.use(express.json())
 app.use(cors())
@@ -15,6 +16,23 @@ app.listen(3001,()=>{
 app.post('/login', (req,res) => {
     const {email, password} = req.body;
     UserModel.findOne({email: email})
+    .then(user => {
+        if(user){
+            if(user.password === password) {
+                res.json("Success!")
+            } else {
+                res.json("Wrong Password.")
+            }
+        } else {
+            res.json("No Records Exist.")
+        }
+    })
+    .catch(err => res.json(err))
+})
+
+app.post('/login', (req,res) => {
+    const {email, password} = req.body;
+    AdminModel.findOne({email: email})
     .then(user => {
         if(user){
             if(user.password === password) {
---
+++ models/Admin.js
@@ -0,0 +1,8 @@
+const mongoose = require('mongoose')
+const AdminSchema = new mongoose.Schema({
+    email: String,
+    password: String
+})
+
+const AdminModel = mongoose.model("admins", AdminSchema)
+module.exports = AdminModel
